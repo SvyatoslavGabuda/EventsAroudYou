d34d0c4388d49a0d342337468aaaa42fc4735fde
SvyatoslavGabuda <sgabuda@gmail.com>
2023-06-03 11:06:24 +0200

    commento

--- src/main/java/it/epicode/eaw/dto/EventoDto.java
+++ src/main/java/it/epicode/eaw/dto/EventoDto.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventoDto {
+	//Classe necessaria per creare eventi o modificarne i dati base
 
 	private Double lat;
 	private Double lng;
